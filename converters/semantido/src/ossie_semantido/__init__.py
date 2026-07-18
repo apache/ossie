bc65bdd28053cbc618f8d8499c05b570d40bc691
Dragos Crintea <hikari.labs@icloud.com>
2026-07-18 23:03:52 +0100

    semantido <-> ossie converter

---
+++ converters/semantido/src/ossie_semantido/__init__.py
@@ -0,0 +1,32 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""semantido <> Apache Ossie converter."""
+
+from ossie_semantido.converter_issues import ConverterIssue, ConverterIssueType, ConverterResult
+from ossie_semantido.loaders import load_from_module
+from ossie_semantido.osi_to_semantido import osi_to_semantido_source
+from ossie_semantido.semantido_to_osi import semantic_layer_to_osi
+
+__all__ = [
+    "ConverterIssue",
+    "ConverterIssueType",
+    "ConverterResult",
+    "load_from_module",
+    "osi_to_semantido_source",
+    "semantic_layer_to_osi",
+]
