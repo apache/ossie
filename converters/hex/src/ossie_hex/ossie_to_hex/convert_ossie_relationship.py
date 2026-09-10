hex-inc/apache-ossie

add hex measure, relation assignment

converters/hex/src/ossie_hex/ossie_to_hex/convert_ossie_relationship.py
@@ -15,10 +15,18 @@
 # specific language governing permissions and limitations
 # under the License.
 
+from typing import Any
+
 from ossie import OSIRelationship
 
-from .context import ExportContext
-from .convert_ossie_name import convert_ossie_name
+from ..hex import HexEntityId, HexRelation, HexRelationType
+from .context import (
+    RELATIONSHIP_DIRECTIONS,
+    ExportContext,
+    RelationshipAnalysis,
+    RelationshipAnalysisEdge,
+    RelationshipAssignment,
+)
 
 # ASSUMPTION: it's kind of ambiguous whether the spec is defining "columns" as
 # physical or logical columns (i.e. a column on the underlying table/view/query
@@ -28,16 +36,53 @@ from .convert_ossie_name import convert_ossie_name
 # syntax (${}) like the relation_id
 
 
+def analyze_ossie_relationship(
+    ossie_relationship: OSIRelationship,
+    *,
+    ctx: ExportContext,
+) -> RelationshipAnalysis | None:
+    """Return both possible directions of a valid Ossie relationship."""
+
+    with ctx.problem_scope(ossie_relationship.name):
+        if (
+            ctx.hex_ids.for_relationship(ossie_relationship.name) is None
+            or ctx.hex_ids.for_dataset(ossie_relationship.from_dataset) is None
+            or ctx.hex_ids.for_dataset(ossie_relationship.to) is None
+        ):
+            return None
+
+        edges: list[RelationshipAnalysisEdge] = []
+        for direction in RELATIONSHIP_DIRECTIONS:
+            if direction == "from_to":
+                source = ossie_relationship.from_dataset
+                target = ossie_relationship.to
+            else:
+                source = ossie_relationship.to
+                target = ossie_relationship.from_dataset
+            edges.append(
+                RelationshipAnalysisEdge(
+                    direction=direction,
+                    source=source,
+                    target=target,
+                )
+            )
+        analysis = RelationshipAnalysis(
+            name=ossie_relationship.name,
+            edges=edges,
+        )
+        return analysis
+
+
 def convert_ossie_relationship(
     ossie_relationship: OSIRelationship,
     *,
     ctx: ExportContext,
-) -> None:
+) -> list[tuple[HexRelation, HexEntityId]] | None:
     """Convert and assign every planned direction of a relationship."""
 
     with ctx.problem_scope(ossie_relationship.name):
-        with ctx.problem_scope("name"):
-            convert_ossie_name(ossie_relationship.name, ctx=ctx)
+        hex_relation_id = ctx.hex_ids.for_relationship(ossie_relationship.name)
+        results: list[tuple[HexRelation, HexEntityId]] = []
 
         with ctx.problem_scope("ai_context"):
             if ossie_relationship.ai_context is not None:
@@ -47,7 +92,89 @@ def convert_ossie_relationship(
             if ossie_relationship.custom_extensions is not None:
                 ctx.warn("Not supported")
 
+        if not hex_relation_id:
+            return None
+
+        assignments = ctx.assignment.for_relationship(ossie_relationship.name)
+        if not assignments:
+            ctx.error("No assignments found")
+            return None
+        for assignment in assignments:
+            source_model_id = ctx.hex_ids.for_dataset(assignment.source)
+            if not source_model_id:
+                ctx.error(f"Source model not available: {assignment.source}")
+                continue
+            target_model_id = ctx.hex_ids.for_dataset(assignment.target)
+            if not target_model_id:
+                ctx.error(f"Target model not available: {assignment.target}")
+                continue
+            relation_type = _hex_relation_type(assignment)
+            local_columns, remote_columns = _ordered_columns(
+                ossie_relationship, assignment
+            )
+            join_sql = _hex_join_sql(hex_relation_id, local_columns, remote_columns)
+
+            spec: dict[str, Any] = {
+                "id": hex_relation_id,
+                "type": relation_type,
+                "join_sql": join_sql,
+            }
+            if target_model_id != hex_relation_id:
+                spec["target"] = target_model_id
+            hex_relation = HexRelation(**spec)
+            results.append((hex_relation, source_model_id))
+
         # Attributes not set:
         # - HexRelation.visibility: Ossie does not support this concept
 
+        return results
+
+
+def assign_ossie_relationship(
+    ossie_relationship: OSIRelationship,
+    conversion: list[tuple[HexRelation, str]] | None,
+    *,
+    ctx: ExportContext,
+) -> None:
+    if conversion is None:
         return
+    with ctx.problem_scope(ossie_relationship.name):
+        for hex_relation, hex_model_id in conversion:
+            hex_model = ctx.hex_model_by_id(hex_model_id)
+            if hex_model is None:
+                ctx.error(
+                    f"Unable to find model to attach relation. Model: {hex_model_id}"
+                )
+                return
+            hex_model.relations.append(hex_relation)
+
+
+def _hex_relation_type(assignment: RelationshipAssignment) -> HexRelationType:
+    if assignment.direction == "from_to":
+        return HexRelationType.MANY_TO_ONE
+    else:
+        return HexRelationType.ONE_TO_MANY
+
+
+def _ordered_columns(
+    ossie_relationship: OSIRelationship, assignment: RelationshipAssignment
+) -> tuple[list[str], list[str]]:
+    if assignment.direction == "from_to":
+        local_columns = ossie_relationship.from_columns
+        remote_columns = ossie_relationship.to_columns
+    else:
+        local_columns = ossie_relationship.to_columns
+        remote_columns = ossie_relationship.from_columns
+    return local_columns, remote_columns
+
+
+def _hex_join_sql(
+    hex_relation_id: HexEntityId,
+    local_columns: list[str],
+    remote_columns: list[str],
+) -> str:
+    join_sql = " AND ".join(
+        f"{local} = ${{{hex_relation_id}}}.{remote}"
+        for local, remote in zip(local_columns, remote_columns, strict=True)
+    )
+    return join_sql

converters/hex/src/ossie_hex/ossie_to_hex/convert_ossie_semantic_model.py
@@ -20,10 +20,20 @@ from __future__ import annotations
 from ossie import OSISemanticModel
 
 from ..hex import HexProject, HexResource
+from .build_assignments import build_assignments
 from .context import ExportContext
 from .convert_ossie_dataset import convert_ossie_dataset
-from .convert_ossie_metric import convert_ossie_metric
-from .convert_ossie_relationship import convert_ossie_relationship
+from .convert_ossie_metric import (
+    analyze_ossie_metric,
+    assign_ossie_metric,
+    convert_ossie_metric,
+)
+from .convert_ossie_name import convert_ossie_name
+from .convert_ossie_relationship import (
+    analyze_ossie_relationship,
+    assign_ossie_relationship,
+    convert_ossie_relationship,
+)
 
 
 def convert_ossie_semantic_model(
@@ -36,6 +46,8 @@ def convert_ossie_semantic_model(
     Returns the converted Hex project.
     """
     with ctx.semantic_model_scope(ossie_semantic_model.name):
+        _store_converted_names(ossie_semantic_model, ctx=ctx)
+
         with ctx.problem_scope("datasets"):
             for ossie_dataset in ossie_semantic_model.datasets:
                 hex_model = convert_ossie_dataset(ossie_dataset, ctx=ctx)
@@ -43,11 +55,25 @@ def convert_ossie_semantic_model(
 
         with ctx.problem_scope("relationships"):
             for ossie_relationship in ossie_semantic_model.relationships or []:
-                convert_ossie_relationship(ossie_relationship, ctx=ctx)
+                analysis = analyze_ossie_relationship(ossie_relationship, ctx=ctx)
+                ctx.analysis.set_for_relationship(analysis)
 
         with ctx.problem_scope("metrics"):
             for ossie_metric in ossie_semantic_model.metrics or []:
-                convert_ossie_metric(ossie_metric, ctx=ctx)
+                analysis = analyze_ossie_metric(ossie_metric, ctx=ctx)
+                ctx.analysis.set_for_metric(analysis)
+
+        build_assignments(ctx=ctx)
+
+        with ctx.problem_scope("relationships"):
+            for ossie_relationship in ossie_semantic_model.relationships or []:
+                conversion = convert_ossie_relationship(ossie_relationship, ctx=ctx)
+                assign_ossie_relationship(ossie_relationship, conversion, ctx=ctx)
+
+        with ctx.problem_scope("metrics"):
+            for ossie_metric in ossie_semantic_model.metrics or []:
+                conversion = convert_ossie_metric(ossie_metric, ctx=ctx)
+                assign_ossie_metric(ossie_metric, conversion, ctx=ctx)
 
         with ctx.problem_scope("description"):
             if ossie_semantic_model.description is not None:
@@ -72,3 +98,26 @@ def convert_ossie_semantic_model(
         )
 
     return hex_project
+
+
+def _store_converted_names(
+    ossie_semantic_model: OSISemanticModel,
+    *,
+    ctx: ExportContext,
+) -> None:
+    for d in ossie_semantic_model.datasets:
+        with ctx.problem_scope("datasets", d.name):
+            if id := convert_ossie_name(d.name, ctx=ctx):
+                ctx.hex_ids.set_for_dataset(d.name, id)
+        for f in d.fields or []:
+            with ctx.problem_scope("datasets", d.name, "fields", f.name):
+                if id := convert_ossie_name(f.name, ctx=ctx):
+                    ctx.hex_ids.set_for_field(d.name, f.name, id)
+    for m in ossie_semantic_model.metrics or []:
+        with ctx.problem_scope("metrics", m.name):
+            if id := convert_ossie_name(m.name, ctx=ctx):
+                ctx.hex_ids.set_for_metric(m.name, id)
+    for r in ossie_semantic_model.relationships or []:
+        with ctx.problem_scope("relationships", r.name):
+            if id := convert_ossie_name(r.name, ctx=ctx):
+                ctx.hex_ids.set_for_relationship(r.name, id)

apache/ossie

converters/hex/src/ossie_hex/ossie_to_hex/convert_ossie_relationship.py
@@ -15,10 +15,18 @@
 # specific language governing permissions and limitations
 # under the License.
 
+from typing import Any
+
 from ossie import OssieRelationship
 
-from .context import ExportContext
-from .convert_ossie_name import convert_ossie_name
+from ..hex import HexEntityId, HexRelation, HexRelationType
+from .context import (
+    RELATIONSHIP_DIRECTIONS,
+    ExportContext,
+    RelationshipAnalysis,
+    RelationshipAnalysisEdge,
+    RelationshipAssignment,
+)
 
 # ASSUMPTION: it's kind of ambiguous whether the spec is defining "columns" as
 # physical or logical columns (i.e. a column on the underlying table/view/query
@@ -28,16 +36,53 @@ from .convert_ossie_name import convert_ossie_name
 # syntax (${}) like the relation_id
 
 
+def analyze_ossie_relationship(
+    ossie_relationship: OssieRelationship,
+    *,
+    ctx: ExportContext,
+) -> RelationshipAnalysis | None:
+    """Return both possible directions of a valid Ossie relationship."""
+
+    with ctx.problem_scope(ossie_relationship.name):
+        if (
+            ctx.hex_ids.for_relationship(ossie_relationship.name) is None
+            or ctx.hex_ids.for_dataset(ossie_relationship.from_dataset) is None
+            or ctx.hex_ids.for_dataset(ossie_relationship.to) is None
+        ):
+            return None
+
+        edges: list[RelationshipAnalysisEdge] = []
+        for direction in RELATIONSHIP_DIRECTIONS:
+            if direction == "from_to":
+                source = ossie_relationship.from_dataset
+                target = ossie_relationship.to
+            else:
+                source = ossie_relationship.to
+                target = ossie_relationship.from_dataset
+            edges.append(
+                RelationshipAnalysisEdge(
+                    direction=direction,
+                    source=source,
+                    target=target,
+                )
+            )
+        analysis = RelationshipAnalysis(
+            name=ossie_relationship.name,
+            edges=edges,
+        )
+        return analysis
+
+
 def convert_ossie_relationship(
     ossie_relationship: OssieRelationship,
     *,
     ctx: ExportContext,
-) -> None:
+) -> list[tuple[HexRelation, HexEntityId]] | None:
     """Convert and assign every planned direction of a relationship."""
 
     with ctx.problem_scope(ossie_relationship.name):
-        with ctx.problem_scope("name"):
-            convert_ossie_name(ossie_relationship.name, ctx=ctx)
+        hex_relation_id = ctx.hex_ids.for_relationship(ossie_relationship.name)
+        results: list[tuple[HexRelation, HexEntityId]] = []
 
         with ctx.problem_scope("ai_context"):
             if ossie_relationship.ai_context is not None:
@@ -47,7 +92,89 @@ def convert_ossie_relationship(
             if ossie_relationship.custom_extensions is not None:
                 ctx.warn("Not supported")
 
+        if not hex_relation_id:
+            return None
+
+        assignments = ctx.assignment.for_relationship(ossie_relationship.name)
+        if not assignments:
+            ctx.error("No assignments found")
+            return None
+        for assignment in assignments:
+            source_model_id = ctx.hex_ids.for_dataset(assignment.source)
+            if not source_model_id:
+                ctx.error(f"Source model not available: {assignment.source}")
+                continue
+            target_model_id = ctx.hex_ids.for_dataset(assignment.target)
+            if not target_model_id:
+                ctx.error(f"Target model not available: {assignment.target}")
+                continue
+            relation_type = _hex_relation_type(assignment)
+            local_columns, remote_columns = _ordered_columns(
+                ossie_relationship, assignment
+            )
+            join_sql = _hex_join_sql(hex_relation_id, local_columns, remote_columns)
+
+            spec: dict[str, Any] = {
+                "id": hex_relation_id,
+                "type": relation_type,
+                "join_sql": join_sql,
+            }
+            if target_model_id != hex_relation_id:
+                spec["target"] = target_model_id
+            hex_relation = HexRelation(**spec)
+            results.append((hex_relation, source_model_id))
+
         # Attributes not set:
         # - HexRelation.visibility: Ossie does not support this concept
 
+        return results
+
+
+def assign_ossie_relationship(
+    ossie_relationship: OssieRelationship,
+    conversion: list[tuple[HexRelation, str]] | None,
+    *,
+    ctx: ExportContext,
+) -> None:
+    if conversion is None:
         return
+    with ctx.problem_scope(ossie_relationship.name):
+        for hex_relation, hex_model_id in conversion:
+            hex_model = ctx.hex_model_by_id(hex_model_id)
+            if hex_model is None:
+                ctx.error(
+                    f"Unable to find model to attach relation. Model: {hex_model_id}"
+                )
+                return
+            hex_model.relations.append(hex_relation)
+
+
+def _hex_relation_type(assignment: RelationshipAssignment) -> HexRelationType:
+    if assignment.direction == "from_to":
+        return HexRelationType.MANY_TO_ONE
+    else:
+        return HexRelationType.ONE_TO_MANY
+
+
+def _ordered_columns(
+    ossie_relationship: OssieRelationship, assignment: RelationshipAssignment
+) -> tuple[list[str], list[str]]:
+    if assignment.direction == "from_to":
+        local_columns = ossie_relationship.from_columns
+        remote_columns = ossie_relationship.to_columns
+    else:
+        local_columns = ossie_relationship.to_columns
+        remote_columns = ossie_relationship.from_columns
+    return local_columns, remote_columns
+
+
+def _hex_join_sql(
+    hex_relation_id: HexEntityId,
+    local_columns: list[str],
+    remote_columns: list[str],
+) -> str:
+    join_sql = " AND ".join(
+        f"{local} = ${{{hex_relation_id}}}.{remote}"
+        for local, remote in zip(local_columns, remote_columns, strict=True)
+    )
+    return join_sql

converters/hex/src/ossie_hex/ossie_to_hex/convert_ossie_semantic_model.py
@@ -20,10 +20,20 @@ from __future__ import annotations
 from ossie import OssieSemanticModel
 
 from ..hex import HexProject, HexResource
+from .build_assignments import build_assignments
 from .context import ExportContext
 from .convert_ossie_dataset import convert_ossie_dataset
-from .convert_ossie_metric import convert_ossie_metric
-from .convert_ossie_relationship import convert_ossie_relationship
+from .convert_ossie_metric import (
+    analyze_ossie_metric,
+    assign_ossie_metric,
+    convert_ossie_metric,
+)
+from .convert_ossie_name import convert_ossie_name
+from .convert_ossie_relationship import (
+    analyze_ossie_relationship,
+    assign_ossie_relationship,
+    convert_ossie_relationship,
+)
 
 
 def convert_ossie_semantic_model(
@@ -36,6 +46,8 @@ def convert_ossie_semantic_model(
     Returns the converted Hex project.
     """
     with ctx.semantic_model_scope(ossie_semantic_model.name):
+        _store_converted_names(ossie_semantic_model, ctx=ctx)
+
         with ctx.problem_scope("datasets"):
             for ossie_dataset in ossie_semantic_model.datasets:
                 hex_model = convert_ossie_dataset(ossie_dataset, ctx=ctx)
@@ -43,11 +55,25 @@ def convert_ossie_semantic_model(
 
         with ctx.problem_scope("relationships"):
             for ossie_relationship in ossie_semantic_model.relationships or []:
-                convert_ossie_relationship(ossie_relationship, ctx=ctx)
+                analysis = analyze_ossie_relationship(ossie_relationship, ctx=ctx)
+                ctx.analysis.set_for_relationship(analysis)
 
         with ctx.problem_scope("metrics"):
             for ossie_metric in ossie_semantic_model.metrics or []:
-                convert_ossie_metric(ossie_metric, ctx=ctx)
+                analysis = analyze_ossie_metric(ossie_metric, ctx=ctx)
+                ctx.analysis.set_for_metric(analysis)
+
+        build_assignments(ctx=ctx)
+
+        with ctx.problem_scope("relationships"):
+            for ossie_relationship in ossie_semantic_model.relationships or []:
+                conversion = convert_ossie_relationship(ossie_relationship, ctx=ctx)
+                assign_ossie_relationship(ossie_relationship, conversion, ctx=ctx)
+
+        with ctx.problem_scope("metrics"):
+            for ossie_metric in ossie_semantic_model.metrics or []:
+                conversion = convert_ossie_metric(ossie_metric, ctx=ctx)
+                assign_ossie_metric(ossie_metric, conversion, ctx=ctx)
 
         with ctx.problem_scope("description"):
             if ossie_semantic_model.description is not None:
@@ -72,3 +98,26 @@ def convert_ossie_semantic_model(
         )
 
     return hex_project
+
+
+def _store_converted_names(
+    ossie_semantic_model: OssieSemanticModel,
+    *,
+    ctx: ExportContext,
+) -> None:
+    for d in ossie_semantic_model.datasets:
+        with ctx.problem_scope("datasets", d.name):
+            if id := convert_ossie_name(d.name, ctx=ctx):
+                ctx.hex_ids.set_for_dataset(d.name, id)
+        for f in d.fields or []:
+            with ctx.problem_scope("datasets", d.name, "fields", f.name):
+                if id := convert_ossie_name(f.name, ctx=ctx):
+                    ctx.hex_ids.set_for_field(d.name, f.name, id)
+    for m in ossie_semantic_model.metrics or []:
+        with ctx.problem_scope("metrics", m.name):
+            if id := convert_ossie_name(m.name, ctx=ctx):
+                ctx.hex_ids.set_for_metric(m.name, id)
+    for r in ossie_semantic_model.relationships or []:
+        with ctx.problem_scope("relationships", r.name):
+            if id := convert_ossie_name(r.name, ctx=ctx):
+                ctx.hex_ids.set_for_relationship(r.name, id)