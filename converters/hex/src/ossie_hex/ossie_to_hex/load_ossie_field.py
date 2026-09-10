hex-inc/apache-ossie

prettify cli output

converters/hex/src/ossie_hex/ossie_to_hex/load_ossie_field.py
@@ -32,7 +32,9 @@ def load_ossie_field(
             expression = load_ossie_field_expression(field.expression, ctx=ctx)
         with ctx.problem_scope("datatype"):
             datatype = load_ossie_datatype(
-                field.datatype, default=OSIDataType.STRING, ctx=ctx
+                field.datatype,
+                default=OSIDataType.STRING,
+                ctx=ctx,
             )
 
     if expression is None:

converters/hex/src/ossie_hex/ossie_to_hex/load_ossie_metric.py
@@ -35,7 +35,9 @@ def load_ossie_metric(
             )
         with ctx.problem_scope("datatype"):
             datatype = load_ossie_datatype(
-                metric.datatype, default=OSIDataType.DECIMAL, ctx=ctx
+                metric.datatype,
+                default=OSIDataType.DECIMAL,
+                ctx=ctx,
             )
 
     if not expression:

apache/ossie

converters/hex/src/ossie_hex/ossie_to_hex/load_ossie_field.py
@@ -32,7 +32,9 @@ def load_ossie_field(
             expression = load_ossie_field_expression(field.expression, ctx=ctx)
         with ctx.problem_scope("datatype"):
             datatype = load_ossie_datatype(
-                field.datatype, default=OssieDataType.STRING, ctx=ctx
+                field.datatype,
+                default=OssieDataType.STRING,
+                ctx=ctx,
             )
 
     if expression is None:

converters/hex/src/ossie_hex/ossie_to_hex/load_ossie_metric.py
@@ -35,7 +35,9 @@ def load_ossie_metric(
             )
         with ctx.problem_scope("datatype"):
             datatype = load_ossie_datatype(
-                metric.datatype, default=OssieDataType.DECIMAL, ctx=ctx
+                metric.datatype,
+                default=OssieDataType.DECIMAL,
+                ctx=ctx,
             )
 
     if not expression: